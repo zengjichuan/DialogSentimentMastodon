f5a6c8a8c902914fb47d2f3cdeadb22f9b72be39
xtof <cerisara@loria.fr>
2018-08-18 12:43:46 +0200

    bugfix print dialog

--- code/corpus/editDialogs.py
+++ code/corpus/editDialogs.py
@@ -6,6 +6,9 @@
 import io
 import json
 
+def dprint(s):
+    print(s+'<br>')
+
 with io.open("dialogs.idx","r",encoding="utf-8") as f: lines=f.readlines()
 
 allids=[]
@@ -14,7 +17,7 @@ for l in lines:
     allids+=ids
 setids=set(allids)
 assert len(setids)==len(allids)
-print("nposts in dialogs",len(setids))
+dprint("nposts in dialogs "+str(len(setids)))
 
 toots = {}
 with io.open("toots.txt","r",encoding="utf-8") as f:
@@ -27,16 +30,22 @@ with io.open("cleaned.txt","r",encoding="utf-8") as f:
         res=json.loads(l)
         for t in res:
             if int(t['id']) in setids: toots[int(t['id'])]=t
-print("ntoots",len(toots))
+dprint("ntoots "+str(len(toots)))
 
 def recurs(sons,cur,l):
     s='..'*l
-    print(s+toots[cur]['account']['username']+": "+toots[cur]['content'])
+    try:
+        dprint(s+toots[cur]['account']['username']+": "+toots[cur]['content'])
+    except: dprint("unicode issue")
     for son in sons[cur]:
         recurs(sons,son,l+2)
 
 for l in lines:
+    # l contains all the IDs in one dialog: warning: some toots with these IDs may not have been downloaded !
     ids=[int(s) for s in l.strip().split()]
+    # so we must remove them
+    presentids = [i for i in ids if i in toots]
+    ids=presentids
    
     # remove monologues 
     authors=[toots[i]['account']['username'] for i in ids]
@@ -48,7 +57,10 @@ for l in lines:
     while True:
         if not 'in_reply_to_id' in toots[root]: break
         if toots[root]['in_reply_to_id']==None: break
-        root=int(toots[root]['in_reply_to_id'])
+        root0=int(toots[root]['in_reply_to_id'])
+        # if the root has not been downloaded, cut the dialog at the last downloaded toot
+        if not root0 in toots: break
+        root=root0
     sons={}
     for id in ids: sons[id]=[]
     for id in ids:
@@ -56,6 +68,8 @@ for l in lines:
         par = toot['in_reply_to_id']
         if not par==None:
             parid = int(par)
+            # if the root has not been downloaded, cut the dialog at the last downloaded toot
+            if not parid in toots: break
             sons[parid].append(id)
         else: assert id==root
     recurs(sons,root,0)
